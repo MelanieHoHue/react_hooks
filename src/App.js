589ffe5f7185e3547289843378cee4442e09d693
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-08-19 20:43:47 +0200

    chapter3_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
+import PostList from './post/PostList'
+import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
 
+const user = 'Melanie'
+const posts = [{
+  title: 'Post1',
+  content: 'bla',
+  author: 'me'
+}, {
+  title: 'Post2',
+  content: 'bla',
+  author: 'me'
+}]
+
 export default function App () {
-  return <UserBar />
+  return (
+    <div style={{ padding: 8 }}>
+      <UserBar />
+      <br />
+      <CreatePost user={user} />
+      <br/>
+      <hr/>
+      <PostList posts={posts} />
+    </div>
+  )
 }
