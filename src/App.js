9d6dda1a886621c1f441c2679c8fcfb4cadbb4e5
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-08-20 19:06:02 +0200

    chapter4_3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useReducer } from 'react'
+import React, {useState, useReducer, useEffect } from 'react'
 
 import appReducer from './reducers'
 
@@ -20,6 +20,14 @@ export default function App () {
 
   const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
   const { user, posts } = state
+
+  useEffect(() => {
+    if (user) {
+      document.title = `${user} - React Hooks Blog`
+    } else {
+      document.title = 'React Hooks Blog'
+    }
+  }, [user])
 
   return (
     <div style={{ padding: 8 }}>
