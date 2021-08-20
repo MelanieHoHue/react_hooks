72e01a20787d4b01a94cfa80063c07c249b95d2f
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-08-20 09:22:00 +0200

    chapter4_0: reducer example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState } from 'react'
+import React, {useState, useReducer } from 'react'
 
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
+import { act } from 'react-dom/cjs/react-dom-test-utils.production.min'
 
 const defaultPosts = [{
   title: 'Post1',
@@ -18,7 +19,33 @@ export default function App () {
 
   const [ user, setUser ] = useState('')
   const [ posts, setPosts ] = useState(defaultPosts)
+
+  function reducer (state, action) {
+    switch (action.type) {
+      case 'TOOGLE_EXPAND':
+        return { ...state, expandPosts: !state.expandPosts}
+      case 'CHANGE_FILTER':
+        let filter = typeof state.filter === 'object' ? state.filter : {}  
+        if (action.all) {
+          return {...state, filter: 'all'}
+        }
+        if (action.fromDate) {
+          filter = { ...filter, fromDate: action.fromDate }
+        }
+        if (action.byAuthor) {
+          filter = { ...filter, byAuthor: action.byAuthor }
+        }
+        return { ...state, filter }
+      default:
+        throw new Error()
+    }
+  }
   
+  const initialState = { all: true }
+  const [ state, dispatch ] = useReducer(reducer, initialState)
+
+  dispatch({ type: 'TOGGLE_EXPAND' })
+
   return (
     <div style={{ padding: 8 }}>
       <UserBar user={user} setUser={setUser}/>
