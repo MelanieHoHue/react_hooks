ddeb78b60ae2bd528b880ca3d85cca02a0df4c2d
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-08-21 08:18:36 +0200

    chapter5_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {useState, useReducer, useEffect } from 'react'
 
 import appReducer from './reducers'
+import { ThemeContext } from './contexts'
 
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
 import Header from './Header'
+import ChangeTheme from './ChangeTheme'
+
+const appConfig = {
+  title: 'React Hooks Blog'
+}
 
 const defaultPosts = [{
   title: 'Post1',
@@ -17,36 +23,35 @@ const defaultPosts = [{
   author: 'me'
 }]
 
-export const ThemeContext = React.createContext({ primaryColor: 'deepskyblue'})
-
 export default function App () {
-
+  const [ theme, setTheme ] = useState({ 
+    primaryColor: 'deepskyblue',
+    secondaryColor: 'coral'
+  })
   const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
   const { user, posts } = state
 
   useEffect(() => {
     if (user) {
-      document.title = `${user} - React Hooks Blog`
+      document.title = `${user} - ${appConfig.title}`
     } else {
-      document.title = 'React Hooks Blog'
+      document.title = appConfig.title
     }
   }, [user])
 
   return (
-    <div style={{ padding: 8 }}>
-      <ThemeContext.Provider value={{ primaryColor: 'coral' }}>
-        <Header text="Hello World" />
-        <ThemeContext.Provider value={{ primaryColor: 'deepskyblue' }}>
-          <Header text="This is a test" />
-        </ThemeContext.Provider>
-      </ThemeContext.Provider>
-
-      <UserBar user={user} dispatch={dispatch}/>
-      <br />
-      {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
-      <br/>
-      <hr/>
-      <PostList posts={posts} />
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div style={{ padding: 8 }}>
+        <Header text={appConfig.title} />
+        <ChangeTheme theme={theme} setTheme={setTheme} />
+        <br/>
+        <UserBar user={user} dispatch={dispatch}/>
+        <br />
+        {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
+        <br/>
+        <hr/>
+        <PostList posts={posts} />
+      </div>
+    </ThemeContext.Provider>
   )
 }
